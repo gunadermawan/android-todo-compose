1e99d1d7ef848347d2fef6ef525daf1d1f4ee3dc
gunadermawan <gunadermawan.official@gmail.com>
2023-03-09 22:31:22 +0700

    setup room db and update gradle version

--- app/src/main/java/com/gunder/todocompose/data/models/ToDoTask.kt
+++ app/src/main/java/com/gunder/todocompose/data/models/ToDoTask.kt
@@ -1,13 +1,13 @@
-package com.gunder.todocompose.models
+package com.gunder.todocompose.data.models
 
 
 import androidx.room.Entity
 import com.gunder.todocompose.utils.Constant.DATABASE_NAME
 
-@Entity(tableName = DATABASE_NAME )
+@Entity(tableName = DATABASE_NAME)
 data class ToDoTask(
     val id: Int = 0,
     val title: String,
     val message: String,
-    val priority: Priority
+    val priority: Priority,
 )
--- app/src/main/java/com/gunder/todocompose/ui/theme/Color.kt
+++ app/src/main/java/com/gunder/todocompose/ui/theme/Color.kt
@@ -6,3 +6,8 @@ val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+
+val HighPriorityColor = Color(android.graphics.Color.RED)
+val MediumPriorityColor = Color(android.graphics.Color.YELLOW)
+val LowPriorityColor = Color(android.graphics.Color.GREEN)
+val NonePriorityColor = Color(android.graphics.Color.WHITE)
