3620dd1f10225b13e41e137f963fcf20b0eed30c
gunadermawan <gunadermawan.official@gmail.com>
2023-03-22 23:02:27 +0700

    create todo repository

---
+++ app/src/main/java/com/gunder/todocompose/data/repositories/ToDoRepository.kt
@@ -0,0 +1,35 @@
+package com.gunder.todocompose.data.repositories
+
+import com.gunder.todocompose.data.ToDoDao
+import com.gunder.todocompose.data.models.ToDoTask
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class ToDoRepository @Inject constructor(private val toDoDao: ToDoDao) {
+    val getAllTask: Flow<List<ToDoTask>> = toDoDao.getAllTask()
+    val sortByLowPriority: Flow<List<ToDoTask>> = toDoDao.sortByLowPriority()
+    val sortByHighPriority: Flow<List<ToDoTask>> = toDoDao.sortByHighPriority()
+    fun getSelectedTask(taskId: Int): Flow<ToDoTask> {
+        return toDoDao.getSelectedTask(taskId = taskId)
+    }
+
+    suspend fun addTask(toDoTask: ToDoTask) {
+        toDoDao.addTask(toDoTask = toDoTask)
+    }
+
+    suspend fun updateTask(toDoTask: ToDoTask) {
+        toDoDao.updateTask(toDoTask = toDoTask)
+    }
+
+    suspend fun deleteTask(toDoTask: ToDoTask) {
+        toDoDao.deleteTask(toDoTask = toDoTask)
+    }
+
+    suspend fun deleteAllTask() {
+        toDoDao.deleteAllTask()
+    }
+
+    fun searchDatabase(searchQuery: String) {
+        toDoDao.searchDatabase(keyword = searchQuery)
+    }
+}
